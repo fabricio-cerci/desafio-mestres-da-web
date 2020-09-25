39c2f7cdfea3ad1fa3e62b8d0bc687062ef6eed3
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-25 15:16:09 -0400

    Implements product attribute value model

---
+++ src/models/ProductAttributeValue.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import ProductAttribute from './ProductAttribute';
+
+@Entity('product_attribute_values')
+class ProductAttributeValue {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  value: string;
+
+  @Column()
+  product_attribute_id: string;
+
+  @ManyToOne(() => ProductAttribute)
+  @JoinColumn({ name: 'product_attribute_id' })
+  product_attribute: ProductAttribute;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default ProductAttributeValue;
