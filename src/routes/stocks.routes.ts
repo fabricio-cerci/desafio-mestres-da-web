59602c49e48f70c30f74cee8f93abc4573d41e8e
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-25 17:51:27 -0400

    Implements stocks routes

---
+++ src/routes/stocks.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Stock from '../models/Stock';
+import AddProductToStockService from '../services/AddProductToStockService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ensureIsAdmin from '../middlewares/ensureIsAdmin';
+
+const productsRouter = Router();
+
+productsRouter.use(ensureAuthenticated);
+productsRouter.use(ensureIsAdmin);
+
+productsRouter.get('/', async (request, response) => {
+  const stocksRepository = getRepository(Stock);
+  const stocks = await stocksRepository.find();
+
+  return response.json(stocks);
+});
+
+productsRouter.post('/:id/products', async (request, response) => {
+  const { productIds } = request.body;
+  const { id } = request.params;
+
+  const createProductService = new AddProductToStockService();
+
+  const stock = await createProductService.execute({
+    productIds,
+    stockId: id,
+  });
+
+  return response.json(stock);
+});
+
+export default productsRouter;
