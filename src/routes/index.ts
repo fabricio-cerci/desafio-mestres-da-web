758401136a2c421d2dcd394c7ef3bc46fe004cfe
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-25 15:19:30 -0400

    Implements use of products routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
+import productsRouter from './products.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/products', productsRouter);
 
 export default routes;
