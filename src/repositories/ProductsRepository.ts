7b12c5a1d978269080f7844a0d349d19207c5d7a
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-25 15:18:18 -0400

    Implements Product Repository

---
+++ src/repositories/ProductsRepository.ts
@@ -0,0 +1,16 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Product from '../models/Product';
+
+@EntityRepository(Product)
+class ProductRepository extends Repository<Product> {
+  public async findBySku(sku: string): Promise<Product | null> {
+    const findProduct = await this.findOne({
+      where: { sku },
+    });
+
+    return findProduct || null;
+  }
+}
+
+export default ProductRepository;
