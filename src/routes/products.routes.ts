c7086a20df5f103635ecf29da9c57bbc8489f238
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-25 19:51:25 -0400

    Fix get route and add delete and put routes

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 
 import ProductsRepository from '../repositories/ProductsRepository';
 import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import UpdateProductService from '../services/UpdateProductService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureIsAdmin from '../middlewares/ensureIsAdmin';
 
@@ -11,9 +13,11 @@ const productsRouter = Router();
 productsRouter.use(ensureAuthenticated);
 productsRouter.use(ensureIsAdmin);
 
-productsRouter.get('/', (request, response) => {
+productsRouter.get('/', async (request, response) => {
   const productsRepository = getCustomRepository(ProductsRepository);
-  const products = productsRepository.find();
+  const products = await productsRepository.find({
+    relations: ['brand', 'product_type', 'productAttributeValues'],
+  });
 
   return response.json(products);
 });
@@ -42,6 +46,32 @@ productsRouter.post('/', async (request, response) => {
   });
 
   return response.json(product);
+});
+
+productsRouter.put('/:id', async (request, response) => {
+  const product = request.body;
+  const { id } = request.params;
+
+  const updateProductService = new UpdateProductService();
+
+  const updatedProduct = await updateProductService.execute({
+    productId: id,
+    ...product,
+  });
+
+  return response.json(updatedProduct);
+});
+
+productsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteProductService = new DeleteProductService();
+
+  await deleteProductService.execute({
+    productId: id,
+  });
+
+  return response.json();
 });
 
 export default productsRouter;
