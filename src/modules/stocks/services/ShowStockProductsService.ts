531d78079782e1bfa74a8245c0e0d395618d5ac5
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-26 16:11:46 -0400

    Implements ShowStockProducts service

---
+++ src/modules/stocks/services/ShowStockProductsService.ts
@@ -0,0 +1,32 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IStocksRepository from '@modules/stocks/repositories/IStocksRepository';
+
+import Stock from '../infra/typeorm/entities/Stock';
+
+interface IRequest {
+  id: string;
+}
+
+@injectable()
+class ShowStockProductsService {
+  constructor(
+    @inject('StocksRepository')
+    private stocksRepository: IStocksRepository,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<Stock> {
+    const stock = await this.stocksRepository.findOne(id, {
+      relations: ['productToStocks'],
+    });
+
+    if (!stock) {
+      throw new AppError('Stock does not exist');
+    }
+
+    return stock;
+  }
+}
+
+export default ShowStockProductsService;
