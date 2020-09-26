0114fb62b3b329748608040961563a973483de12
Fabricio Cerci <fabriciocerci@gmail.com>
2020-09-26 16:00:56 -0400

    Change service for depency on abstraction instead of real implementation

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,23 +1,27 @@
-import { getCustomRepository } from 'typeorm';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import ProductsRepository from '../repositories/ProductsRepository';
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 
-interface Request {
+interface IRequest {
   productId: string;
 }
 
+@injectable()
 class DeleteProductService {
-  public async execute({ productId }: Request): Promise<void> {
-    const productsRepository = getCustomRepository(ProductsRepository);
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
-    const product = await productsRepository.findOne(productId);
+  public async execute({ productId }: IRequest): Promise<void> {
+    const product = await this.productsRepository.findById(productId);
 
     if (!product) {
       throw new AppError('Product does not exist');
     }
 
-    await productsRepository.remove(product);
+    await this.productsRepository.remove(product);
   }
 }
 
